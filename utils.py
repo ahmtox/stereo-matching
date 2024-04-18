6544f99b34a27246b971c76c0f8f5db3af000b6d
Juan Pablo Heusser <juanp.heusser@gmail.com>
2024-04-18 16:02:19 -0400

    Implemented stereogram generator

---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class StereogramGenerator:
+
+    def __init__(self):
+        pass
+
+    def generate_stereogram(self, height, width, horizontal_shift):
+
+        image = np.random.rand(height, width)
+        binary_image = (image > 0.5)*1.0
+
+        height_range = [int(height/4), int(height*3/4)]
+        width_range = [int(width/4), int(width*3/4)]
+
+        shifted_area = binary_image[height_range[0]: height_range[1], width_range[0]: width_range[1]]
+        shifted_image = binary_image.copy()
+        shifted_image[height_range[0]: height_range[1], width_range[0] - horizontal_shift: width_range[1] - horizontal_shift] = shifted_area
+
+        self.binary_image = binary_image
+        self.shifted_image = shifted_image
+
+        return shifted_image
+    
+    def plot_stereogram(self):
+        fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+
+        ax[0].imshow(self.binary_image, cmap='gray')
+        ax[0].set_title('Image 1')
+        ax[0].axis('off')
+
+        ax[1].imshow(self.shifted_image, cmap='gray')
+        ax[1].set_title('Image 2')
+        ax[1].axis('off')
+
+        ax[2].imshow(self.shifted_image - self.binary_image, cmap='gray')
+        ax[2].set_title('difference')
+        ax[2].axis('off')
+
+        plt.tight_layout()
+        plt.show()
